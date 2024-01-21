ed711d49c5407891602f79029d3677aee6c67637
Alekshamon <aleksandrahamon@gmail.com>
2024-01-21 19:07:47 +0100

    delete done

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = require("./src/app");
 require("dotenv").config();
 
 const port = process.env.APP_PORT;
+
 console.log("port", port);
+
 app
   .listen(port, () => {
     console.log(`Server is listening on ${port}`);
